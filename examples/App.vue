e8b56929e4dd5abb447466e25a98879e324e6250
Awe <hilongjw@gmail.com>
2017-05-29 10:13:27 +0800

    add: example

---
+++ examples/App.vue
@@ -0,0 +1,48 @@
+<style>
+html {
+  background: #eeeeee;
+}
+
+html, body {
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  font-family: 'Roboto', sans-serif;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+</style>
+
+<template>
+  <div id="app">
+    <RecyclerView
+      :prerender="30"
+      key="mi" 
+      class="mi-list" 
+      :fetch="MiFetch" 
+      :item="MiItem" 
+      :tombstone="MiTomstone"
+    ></RecyclerView>
+  </div>
+</template>
+
+<script>
+import RecyclerView from 'vue-recyclerview'
+import MiItem from './MiItem.vue'
+import MiTomstone from './MiTombstone.vue'
+import MiFetch from './mi-fetch'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      MiFetch: MiFetch,
+      MiItem,
+      MiTomstone
+    }
+  },
+  components: {
+    RecyclerView
+  }
+}
+</script>
